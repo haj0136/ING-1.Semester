ec55112cdaccfb9f4e546f1cb58a68f411ea7a26
Marek Hajdík <hajdik.marek1@gmail.com>
2018-09-21 16:44:44 +0200

    add project CV_1 to BIA

---
+++ BIA/CV_1/CV_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            FindAllPossiblePermutations("ABCDEFGHIJKL", 'A');
+
+            watch.Stop();
+
+            System.Console.WriteLine( "Time elapsed: " + watch.ElapsedMilliseconds + "ms");
+        }
+
+        static void FindAllPossiblePermutations(string cities, char firstNode, int step = 0)
+        {
+            if(step == 0)
+            {
+                cities = cities.Replace(firstNode.ToString(), string.Empty);
+            }
+            //if (step == cities.Length)
+            //{
+            //    Console.WriteLine(firstNode + cities);
+            //}
+
+            foreach (int i in Enumerable.Range(step, cities.Length - step)){
+
+
+                var cities_copy = SwapCharacters(cities, step, i);
+
+                FindAllPossiblePermutations(cities_copy, firstNode, step + 1);
+            }
+        }
+
+        static string SwapCharacters(string value, int position1, int position2)
+        {
+            // Swaps characters in a string.
+            char[] array = value.ToCharArray(); 
+            char temp = array[position1]; 
+            array[position1] = array[position2]; 
+            array[position2] = temp; 
+            return new string(array); 
+        }
+    }
+}
