34b1ffae3d021c0c25908cf46f5961911ffd3b46
Marek Hajdík <hajdik.marek1@gmail.com>
2018-09-21 15:37:34 +0200

    add project CV_1 to PA I

---
+++ PAI/CV_1/CV_1/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV_1
+{
+    class Person
+    {
+
+        public String Name { get; set; }
+        public String Id { get; set; }
+        public String Roles { get; set; }
+        public String FamilyName { get; set; }
+
+    }
+}
---
+++ PAI/CV_1/CV_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NoUrls = 3308;
+            const string URL = "http://name-service.appspot.com/api/v1/names/";
+
+            Task[] tasks = new Task[NoUrls];
+
+            for (int i = 0; i <= NoUrls; i++)
+            {
+                tasks[i] = Task.Factory.StartNew( state =>
+                {
+                    using (var client = new WebClient())
+                    {
+
+                    }
+                }, )
+            }
+
+            Task.WaitAll(tasks);
+
+        }
+    }
+}
