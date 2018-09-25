8a3a26d2b714d296568f77d1db42e2d5aec76e28
maras49 <hajdk@seznam.cz>
2018-09-25 23:39:10 +0200

    MAD I CV_1 complete

--- MAD I/cv-1/cv-1/ForecastRow.cs
+++ MAD I/cv-1/cv-1/ForecastRow.cs
@@ -14,9 +14,19 @@ namespace cv_1
         public Windy Windy { get; set; }
         public Play Play { get; set; }
 
+        public int YesCount { get; set; }
+        public int NoCount { get; set; }
+        public double Supp { get; set; }
+        public double Conf { get; set; }
+
         public override string ToString()
         {
-            return Outlook + ", " + Temperature + ", " + Humidity + ", " + Windy + ", " + Play;
+            string outlook = Outlook == Outlook.None ? "" : Outlook.ToString() + " + ";
+            string temperature = Temperature == Temperature.None ? "" : Temperature.ToString() + " + ";
+            string humidity = Humidity == Humidity.None ? "" : Humidity.ToString() + " + ";
+            string windy = Windy == Windy.None ? "" : Windy.ToString();
+            
+            return outlook + temperature + humidity + windy;
         }
     }
 
@@ -42,8 +52,8 @@ namespace cv_1
     }
     public enum Windy
     {
-        True,
-        False,
+        TRUE,
+        FALSE,
         None
     }
     public enum Play
--- MAD I/cv-1/cv-1/Program.cs
+++ MAD I/cv-1/cv-1/Program.cs
@@ -13,11 +13,9 @@ namespace cv_1
             DataLoader dl = new DataLoader();
             List<ForecastRow> forecastData = dl.LoadData();
             
-            foreach (ForecastRow item in forecastData)
-            {
-                //Console.WriteLine(item.ToString());
-            }
 
+            List<ForecastRow> rules = new List<ForecastRow>();
+ 
             foreach (var outlook in Enum.GetValues(typeof(Outlook)))
             {
                 foreach (var temperature in Enum.GetValues(typeof(Temperature)))
@@ -26,12 +24,64 @@ namespace cv_1
                     {
                         foreach (var windy in Enum.GetValues(typeof(Windy)))
                         {
-
+                            ForecastRow rule = new ForecastRow
+                            {
+                                Outlook = (Outlook)outlook,
+                                Temperature = (Temperature)temperature,
+                                Humidity = (Humidity)humidity,
+                                Windy = (Windy)windy
+                            };
+                            rules.Add(rule);
                         }
                     }
                 }
 
             }
+
+            foreach (ForecastRow item in forecastData)
+            {
+                //Console.WriteLine(item.ToString());
+                foreach (var rule in rules)
+                {
+                    if ((item.Outlook == rule.Outlook && item.Temperature == rule.Temperature && item.Humidity == rule.Humidity && item.Windy == rule.Windy) || 
+                        (item.Outlook == rule.Outlook && item.Temperature == rule.Temperature && item.Humidity == rule.Humidity && rule.Windy == Windy.None) ||
+                        (item.Outlook == rule.Outlook && item.Temperature == rule.Temperature && rule.Humidity == Humidity.None && rule.Windy == Windy.None) ||
+                        (item.Outlook == rule.Outlook && rule.Temperature == Temperature.None && rule.Humidity == Humidity.None && rule.Windy == Windy.None) ||
+                        (rule.Outlook == Outlook.None && rule.Temperature == item.Temperature && rule.Humidity == Humidity.None && rule.Windy == Windy.None) ||
+                        (rule.Outlook == Outlook.None && rule.Temperature == item.Temperature && rule.Humidity == item.Humidity && rule.Windy == Windy.None) ||
+                        (rule.Outlook == Outlook.None && rule.Temperature == item.Temperature && rule.Humidity == item.Humidity && rule.Windy == item.Windy) ||
+                        (rule.Outlook == Outlook.None && rule.Temperature == Temperature.None && rule.Humidity == item.Humidity && rule.Windy == Windy.None) ||
+                        (rule.Outlook == Outlook.None && rule.Temperature == Temperature.None && rule.Humidity == item.Humidity && rule.Windy == item.Windy) ||
+                        (rule.Outlook == Outlook.None && rule.Temperature == Temperature.None && rule.Humidity == Humidity.None && rule.Windy == item.Windy))                     {
+                        if (item.Play == Play.No)
+                            rule.NoCount++;
+                        else
+                            rule.YesCount++;
+
+                    }
+                }
+            }
+
+            foreach (var rule in rules){
+                double r = rule.NoCount + rule.YesCount;
+                double N = forecastData.Count;
+                double Cmax = rule.NoCount < rule.YesCount ? rule.YesCount : rule.NoCount;
+
+                if (N != 0)
+                    rule.Supp = r / N;
+
+                if (r != 0)
+                    rule.Conf = Cmax / r;
+
+                if (rule.YesCount > 0 || rule.NoCount > 0)
+                {
+                    Console.WriteLine(rule.ToString() + " => yes: " + rule.YesCount + "  no: " + rule.NoCount + " supp = " + rule.Supp.ToString("#0.000") + " conf = " + rule.Conf.ToString("#0.000"));
+                }
+            }
+	
+
+	
+            
 
         }
     }
