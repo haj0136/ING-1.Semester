ecb74bb43fa78569ab19c4c5d012dfd064667116
Marek Hajdík <hajdik.marek1@gmail.com>
2018-09-26 11:58:09 +0200

    add MAD CV_2

---
+++ MAD I/CV_2/CV_2/DataLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV_2
+{
+    class DataLoader
+    {
+        public List<Iris> LoadData()
+        {
+            List<Iris> IrisDataSet = new List<Iris>();
+            List<string> rows = LoadCsvFile("iris.data");
+            foreach (string row in rows)
+            {
+                Iris dataRow = new Iris();
+                List<string> attributes = row.Split(',').ToList();
+                dataRow.Sepal_lenght = Convert.ToDouble(attributes[0], CultureInfo.InvariantCulture.NumberFormat);
+                dataRow.Sepal_width = Convert.ToDouble(attributes[1], CultureInfo.InvariantCulture.NumberFormat);
+                dataRow.Petal_lenght = Convert.ToDouble(attributes[2], CultureInfo.InvariantCulture.NumberFormat);
+                dataRow.Petal_width = Convert.ToDouble(attributes[3], CultureInfo.InvariantCulture.NumberFormat);
+                switch (attributes[4])
+                {
+                    case "Iris-setosa": dataRow.Iris_type = Iris_class.Iris_Setosa; break;
+                    case "Iris-versicolor": dataRow.Iris_type = Iris_class.Iris_Versicolour; break;
+                    case "Iris-virginica": dataRow.Iris_type = Iris_class.Iris_Virginica; break;
+                }
+                IrisDataSet.Add(dataRow);
+            }
+            return IrisDataSet;
+        }
+
+        private List<string> LoadCsvFile(string filePath)
+        {
+            var reader = new StreamReader(File.OpenRead(filePath));
+            List<string> searchList = new List<string>();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                searchList.Add(line);
+            }
+
+            return searchList;
+        }
+    }
+}
---
+++ MAD I/CV_2/CV_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV_2
+{
+    // prumerny vektor (mí)
+    // rozptyl var
+    // euklidovské vzdálenost
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataLoader dl = new DataLoader();
+            List<Iris> IrisList = dl.LoadData();
+
+            foreach (var item in IrisList)
+            {
+                
+            }
+        }
+    }
+}
