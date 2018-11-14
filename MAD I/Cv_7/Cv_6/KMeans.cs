a1bcfefd89ad1297e1073c0ba960f8dc5b910365
Marek Hajdík <hajdik.marek1@gmail.com>
2018-11-14 20:39:36 +0100

    MAD add new task

---
+++ MAD I/Cv_7/Cv_6/KMeans.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv_6
+{
+    public class KMeans
+    {
+        private List<Iris> data;
+        /// <summary>
+        /// number of clusters
+        /// </summary>
+        private readonly int k;
+        /// <summary>
+        /// values in array: Sepal lenght; Sepal width; Petal lenght; Petal width;
+        /// </summary>
+        private List<double[]> clusters;
+
+        public KMeans(List<Iris> dataset)
+        {
+            k = 10;
+            data = new List<Iris>(dataset);
+            clusters = new List<double[]>();
+        }
+
+        public List<Iris> GetClusters()
+        {
+            bool changed = true;
+            int maxIterations = k * 20;
+
+            InitializeCentroids();
+
+            for (int i = 0; i < maxIterations; i++)
+            {
+                UpdateMeans();
+                changed = UpdateClusters();
+
+                if (changed == false)
+                    break;
+            }
+
+            return data;
+        }
+
+        private void InitializeCentroids()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < k; i++)
+            {
+                data[i].ClusterIndex = i;
+            }
+            for (int i = k; i < data.Count; i++)
+            {
+                data[i].ClusterIndex = rnd.Next(0, k);
+            }
+        }
+
+        private void UpdateMeans()
+        {
+            var group = data.GroupBy(iris => iris.ClusterIndex).OrderBy(s => s.Key);
+            foreach (var item in group)
+            {
+                var means = new double[4];
+                means[0] = item.ToList().Sum(iris => iris.Sepal_lenght) / item.Count();
+                means[1] = item.ToList().Sum(iris => iris.Sepal_width) / item.Count();
+                //means[2] = item.ToList().Sum(iris => iris.Petal_lenght) / item.Count();
+                //means[3] = item.ToList().Sum(iris => iris.Petal_width) / item.Count();
+                clusters.Add(means);
+            }
+        }
+
+        private bool UpdateClusters()
+        {
+            bool changed = false;
+            double[] distances = new double[k];
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    distances[j] = HelperTools.EuclideanDistance(data[i].ToArray(), clusters[j]);
+                }
+
+                int newClusterId = Array.IndexOf(distances, distances.Min());
+                if (newClusterId != data[i].ClusterIndex)
+                {
+                    changed = true;
+                    data[i].ClusterIndex = newClusterId;
+                }
+            }
+
+            return changed;
+        }
+    }
+}
---
+++ MAD I/Cv_7/Cv_6/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv_6
+{
+    class Program
+    {
+        // 1. k-NN classification
+        //  nahodně rozdělit do 10 skupin po 15 (trenovací x testovací)
+        // zkusit ruzna K
+        static void Main(string[] args)
+        {
+            DataLoader dl = new DataLoader();
+            List<Iris> irisList = dl.LoadData();
+
+            int K = 3;
+
+            var groups = MakeRandomGroups(irisList);
+
+
+
+            for (int i = 0; i < groups.Count; i++)
+            {
+                for (int j = 0; j < groups[i].Count; j++)
+                {
+                    var distances = GetDistances(groups, i, j).ToList();
+                    distances.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+                    var topK = 
+                    
+                } 
+            }
+
+
+            
+            
+        }
+
+        static double EuclideanDistance(double a, double b)
+        {
+            double result = Math.Pow(a - b, 2);
+
+            return Math.Sqrt(result);
+        }
+
+        static List<List<Iris>> MakeRandomGroups(List<Iris> irises)
+        {
+            Random rnd = new Random();
+            var groups = new List<List<Iris>>(10);
+            var originalList = new List<Iris>(irises);
+
+            for (int j = 0; j < groups.Capacity; j++)
+            {
+                groups.Add(new List<Iris>());
+                for (int i = 0; i < 15; i++)
+                {
+                    var randomIndex = rnd.Next(originalList.Count);
+                    groups[j].Add(originalList[randomIndex]);
+                    originalList.RemoveAt(randomIndex);
+                }
+            }
+
+            return groups;
+        }
+
+        static Dictionary<int, double> GetDistances(List<List<Iris>> groups, int actualGroupIndex, int actualIris)
+        {
+            var distances = new Dictionary<int, double>();
+            for (int i = 0; i < groups.Count; i++)
+            {
+                if (i == actualGroupIndex)
+                    continue;
+                for (int j = 0; j < groups[i].Count; j++)
+                {
+                    distances.Add(groups[actualGroupIndex][actualIris].Id, HelperTools.EuclideanDistance(groups[i][j].ToArray(), groups[actualGroupIndex][actualIris].ToArray()));
+                }
+            }
+
+            return distances;
+        }
+
+    }
+    
+}
