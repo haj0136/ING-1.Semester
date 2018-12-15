34022f465dce26e8b47a7deef4f0a55461a6a924
maras49 <hajdk@seznam.cz>
2018-12-15 20:53:08 +0100

    MAD cv_10 fix

--- MAD I/CV_10/CV_10/Program.cs
+++ MAD I/CV_10/CV_10/Program.cs
@@ -83,6 +83,10 @@ namespace CV_10
 
             Console.WriteLine("Average degree: Actors = " + actors.AverageDegree);
             Console.WriteLine("Average degree: Movies = " + movies.AverageDegree);
+            int[] diameterInfo = GetDiameter(FloydWarshallAlgorithm.GetResult(actors.ToMatrix()));
+            Console.WriteLine($"Actors graph diameter = {diameterInfo[2]} | from {actors.NodeList[diameterInfo[0]].Name} to {actors.NodeList[diameterInfo[1]].Name}");
+            diameterInfo = GetDiameter(FloydWarshallAlgorithm.GetResult(movies.ToMatrix()));
+            Console.WriteLine($"Movies graph diameter = {diameterInfo[2]} | from {movies.NodeList[diameterInfo[0]].Name} to {movies.NodeList[diameterInfo[1]].Name}");
             Console.WriteLine();
 
             actors.CalculateAverageDistance(FloydWarshallAlgorithm.GetResult(actors.ToMatrix()));
@@ -91,6 +95,8 @@ namespace CV_10
             {
                 Console.WriteLine($"Average distance of Node:{actor.Name} = {actor.AverageDistance}");
             }
+
+            Console.WriteLine();
 
         }
 
@@ -116,6 +122,26 @@ namespace CV_10
                 g.AverageDegree += node.Degree;
             }
             g.AverageDegree /= g.NodeList.Count;
+        }
+
+        static int[] GetDiameter(double[,] matrix)
+        {
+            // result[0] = i, result[1] = j, result[2] = diameter
+            int[] result = new int[3];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    if(matrix[i,j] > result[2])
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        result[2] = (int)matrix[i, j];
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
